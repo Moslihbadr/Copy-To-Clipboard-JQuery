39ac2f9e844525410575cc397251b305e2915b9f
Badr Moslih <113358493+Moslihbadr@users.noreply.github.com>
2023-03-29 02:05:56 +0000

    Update app.js

--- practice2/app.js
+++ practice2/app.js
@@ -5,11 +5,10 @@ $(document).ready(()=>{
       document.execCommand('copy')
       $('.message').text('copied')
       $('.message').css('transform','translate3d(173px, -37px, 10px)')
-      $('.message').css('display','block')
     }else {
       $('.message').text('write somthing to copy')
-      $('.message').css('display','block')
       $('.message').css('transform','translate3d(95px, -37px, 10px)')
     }
+    $('.message').css('display','block')
   })
 })
